e345030479c016162d374d5bb24869057a7e7bb1
Supratik Basu <mail.supratikbasu@gmail.com>
2020-07-29 16:38:29 +0530

    Change configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Watchflix - A Netflix clone`,
+        short_name: `Watchflix`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#e50914`,
+        theme_color: `#e50914`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
