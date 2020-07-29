432bfa87a67f0c2f47ae09a01015e43cdb5ecccb
Supratik Basu <mail.supratikbasu@gmail.com>
2020-07-29 20:01:20 +0530

    Add firebase auth service

---
+++ src/services/firebase.js
@@ -0,0 +1,51 @@
+
+import app from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firebase-firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+}
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig)
+    this.auth = app.auth()
+  }
+
+  login(email, password) {
+		return this.auth.signInWithEmailAndPassword(email, password)
+	}
+
+	logout() {
+		return this.auth.signOut()
+	}
+
+	async register(name, email, password) {
+		await this.auth.createUserWithEmailAndPassword(email, password)
+		return this.auth.currentUser.updateProfile({
+			displayName: name
+		})
+  }
+  
+  isInitialized() {
+		return new Promise(resolve => {
+			this.auth.onAuthStateChanged(resolve)
+		})
+	}
+
+	getCurrentUsername() {
+		return this.auth.currentUser && this.auth.currentUser.displayName
+	}
+}
+
+// Create a singleton for firebase
+export default new Firebase()
