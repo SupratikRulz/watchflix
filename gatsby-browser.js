ea546db399151d29cecebaa02dc15312bc341a29
Supratik Basu <mail.supratikbasu@gmail.com>
2020-07-31 13:18:10 +0530

    Add loader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const onInitialClientRender = () => {
+  setTimeout(function() {
+    document.getElementById("___loader").style.display = "none"
+  }, 1000)
+}
 
-// You can delete this file if you're not using it
+export {
+  onInitialClientRender
+}
